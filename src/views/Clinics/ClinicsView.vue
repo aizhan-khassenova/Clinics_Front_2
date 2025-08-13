534ae6ee87a11908f0bce0cd705a590f588f7944
Айжан Хасенова <a.hasenova@ddirection.kz>
2025-08-13 16:34:37 +0500

    feat: add modal for confirm deleting clinic

--- src/views/Clinics/ClinicsView.vue
+++ src/views/Clinics/ClinicsView.vue
@@ -19,14 +19,36 @@
                     <td>{{ clinic.id }}</td>
                     <td>{{ clinic.name }}</td>
                     <td>{{ clinic.address }}</td>
-                    <td>
-                        <RouterLink :to="`/clinics/${clinic.id}/update`" class="btn btn-sm btn-outline-primary">
+                    <td class="d-flex flex-row">
+                        <RouterLink :to="`/clinics/${clinic.id}/update`" class="btn btn-sm btn-outline-primary me-2">
                             <i class="bi bi-pencil-fill"></i>
                         </RouterLink>
+                        <button class="btn btn-sm btn-outline-danger" data-bs-toggle="modal"
+                            data-bs-target="#exampleModal" @click="openDeleteModal(clinic.id)">
+                            <i class="bi bi-trash-fill"></i>
+                        </button>
                     </td>
                 </tr>
             </tbody>
         </table>
+
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary" @click="deleteClinic()">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -36,7 +58,8 @@ import axios from 'axios';
 export default {
     data() {
         return {
-            clinics: []
+            clinics: [],
+            selectedClinicId: null
         }
     },
     created() {
@@ -49,6 +72,19 @@ export default {
                 this.clinics = response.data
             } catch (error) {
                 console.error('Ошибка при загрузке клиник: ', error);
+            }
+        },
+        openDeleteModal(id) {
+            this.selectedClinicId = id; // сохраняем ID клиники
+        },
+        async deleteClinic() {
+            const id = this.selectedClinicId;
+            try {
+                await axios.delete(`/api/clinics/${id}`);
+                this.clinics = this.clinics.filter(clinic => clinic.id !== id)
+                this.selectedClinicId = null;
+            } catch (error) {
+                console.error('Ошибка при удалении клиники: ', error);
             }
         }
     }
